66f99d9cf41f35f0d110f9df3fdda15f1eb32824
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-03-28 17:15:04 +0700

    Update cost.js

--- src/pages/cost/cost.js
+++ src/pages/cost/cost.js
@@ -1,1 +1,29 @@
+var items = document.getElementsByClassName("expense-item");
 
+var total = 0;
+
+for (var i = 0; i < items.length; i++) {
+    var priceText = items[i].getElementsByClassName("ei-price")[0].innerText;
+
+    priceText = priceText.replace(",", "");
+    priceText = priceText.replace("đ", "");
+    priceText = priceText.trim();
+
+    var price = parseInt(priceText);
+
+    total = total + price;
+}
+
+console.log(total); // Cần chạy trên trình duyệt
+
+function deleteSelected() {
+    var checkboxes = document.getElementsByClassName("ei-checkbox"); //Trả về 1 list
+
+    for (var i = checkboxes.length - 1; i >= 0; i--) {
+        if (checkboxes[i].checked) {
+            checkboxes[i].parentElement.remove(); // Leo lên phần tử cha rồi xóa
+        }
+    }
+}
+
+// Nên duyệt ngược để tránh lỗi đẩy phần tử
