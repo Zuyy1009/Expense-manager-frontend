67b9b66b86de5628b8a868b3e75d0cf38b288a31
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-10 19:41:30 +0700

    Update register.jsx

--- Frontend Redesign/Frontend/src/login/register.jsx
+++ Frontend Redesign/Frontend/src/login/register.jsx
@@ -8,8 +8,10 @@ export function Register() {
                 <section className={styles['top-sub-empty']}></section>
                 <section className={styles['login-region']}>
                     <h3 style={{fontSize: "20px", marginBottom:"-10px"}}>TẠO TÀI KHOẢN MỚI</h3>
-                    <p>Email</p>
+                    <p>Tên người dùng</p>
                     <input type='text' className={styles['input-field']} />
+                    <p>Email</p>
+                    <input type='email' className={styles['input-field']} />
                     <p>Mật khẩu</p>
                     <input type='password' className={styles['input-field']} />
                     <br />
@@ -21,6 +23,7 @@ export function Register() {
                         height: "30px",
                         borderRadius: "20px",
                         fontSize: "17px",
+                        marginBottom: "15px",
                     }}>
                         <strong>ĐĂNG KÝ</strong>
                     </button>
