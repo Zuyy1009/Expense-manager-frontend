180fb0b5d9a0313e89df3ac080c482915662e5ae
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-10 20:54:39 +0700

    Update stat.jsx

--- Frontend Redesign/Frontend/src/stat/stat.jsx
+++ Frontend Redesign/Frontend/src/stat/stat.jsx
@@ -1,5 +1,11 @@
+import styles from './stat.module.css'
+
 export function Stat() {
     return (
-        <div><h1>Stat Page</h1></div>
+        <div className={styles['outer-boundary']} >
+            <div className={styles['general-stat']} >Tổng quát chung</div>
+            <div className={styles['recent-transactions']} >Giao dịch gần đây</div>
+            <div className={styles['chart']} >Biểu đồ</div>
+        </div>
     )
 }
