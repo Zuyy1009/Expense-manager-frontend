da5819ebf05a90298df968ed2f4481e5eb316b12
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-12 10:27:52 +0700

    Update transactionsList.js

--- Frontend Redesign/PsBackend/storage/transactionsList.js
+++ Frontend Redesign/PsBackend/storage/transactionsList.js
@@ -1,12 +1,10 @@
-import { Eating, Elecbill, Health, Housing, Movement, Otherexpense, Otherincome, Recreation, Salary, Shopping } from "../../Frontend/src/assets/category_icon/category-image";
-
-export let transList = [
+let transList = [
     {
         id: 1,
         type: 'Chi tiêu',
         amount: 110000,
         category: 'Ăn uống',
-        categoryIcon: Eating,
+        categoryIcon: 'http://localhost:8080/api/images/eating.png',
         date: '12-04-2026',
         note: 'Ăn chực ở Loteria',
         dateCreated: '12-04-2026'
@@ -16,7 +14,7 @@ export let transList = [
         type: 'Chi tiêu',
         amount: 68000,
         category: 'Đi lại',
-        categoryIcon: Movement,
+        categoryIcon: 'http://localhost:8080/api/images/movement.png',
         date: '09-04-2026',
         note: 'Bắt xe ra XXXXMall',
         dateCreated: '09-04-2026'
@@ -26,9 +24,14 @@ export let transList = [
         type: 'Thu nhập',
         amount: 12000000,
         category: 'Lương',
-        categoryIcon: Salary,
+        categoryIcon: 'http://localhost:8080/api/images/salary.png',
         date: '31-03-2026',
         note: 'Lương tháng 3',
         dateCreated: '31-03-2026'
     },
 ];
+
+module.exports = { transList };
+
+// Node.js mặc định xử lý các file .js theo chuẩn CommonJS (sử dụng require) 
+// Trong khi câu lệnh import thuộc chuẩn ES Modules (ESM) => Gây lỗi .
