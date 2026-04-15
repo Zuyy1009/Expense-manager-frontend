851f4257f43c9475df3b6209d857c43dd1fd6f59
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-15 15:35:34 +0700

    Update transactions.jsx

--- Frontend Redesign/Frontend/src/transactions/transactions.jsx
+++ Frontend Redesign/Frontend/src/transactions/transactions.jsx
@@ -165,6 +165,159 @@ export function Transactions() {
             .catch(err => console.error("Lỗi khi thêm:", err));
     }
 
+    const [editingId, setEditingId] = useState(null);
+    const [editedNote, setEditedNote] = useState('');
+    const [editedAmount, setEditedAmount] = useState('');
+    const [editedDate, setEditedDate] = useState('');
+    const [editedCategory, setEditedCategory] = useState('Lương');
+
+    // Hàm xử lý khi 1 radio button được chọn
+    const handleRadioChange = (id) => {
+        setEditingId(id);
+        const itemToEdit = transList.find(item => item.id === id);
+        if (itemToEdit) {
+            // Đổ dữ liệu hiện tại vào form
+            setEditedNote(itemToEdit.note);
+            setEditedAmount(itemToEdit.amount);
+            setEditedDate(itemToEdit.date.split('-').reverse().join('-'));
+            setTypeSelect(itemToEdit.type === 'Thu nhập' ? 'income' : 'expense');
+            setEditedCategory(itemToEdit.category);
+        }
+    };
+
+    useEffect(() => {
+        if (addEdit === 2 && editingId) { // Chỉ chạy khi đang ở chế độ sửa
+            if (typeSelect === 'expense') {
+                setEditedCategory('Ăn uống');
+            } else {
+                setEditedCategory('Lương');
+            }
+        }
+    }, [typeSelect, addEdit]);
+
+    const handleConfirmEdit = () => {
+        if (!editingId) {
+            alert("Vui lòng chọn giao dịch để sửa!")
+            return;
+        }
+
+        // Tái sử dụng logic lấy icon
+
+        let chosenCategoryIcon = '';
+
+        switch (editedCategory) {
+            case 'Ăn uống':
+                chosenCategoryIcon = 'eating';
+                break;
+            case 'Đơn điện tử':
+                chosenCategoryIcon = 'elecbill';
+                break;
+            case 'Sức khỏe':
+                chosenCategoryIcon = 'health';
+                break;
+            case 'Nhà ở':
+                chosenCategoryIcon = 'housing';
+                break;
+            case 'Đi lại':
+                chosenCategoryIcon = 'movement';
+                break;
+            case 'Giải trí':
+                chosenCategoryIcon = 'recreation';
+                break;
+            case 'Mua sắm':
+                chosenCategoryIcon = 'shopping';
+                break;
+            case 'Chi tiêu khác':
+                chosenCategoryIcon = 'otherexpense';
+                break;
+            case 'Lương':
+                chosenCategoryIcon = 'salary';
+                break;
+            case 'Thu nhập khác':
+                chosenCategoryIcon = 'otherincome';
+                break;
+            default:
+                chosenCategoryIcon = 'salary';
+        }
+
+        const updatedTransaction = {
+            id: editingId,
+            note: editedNote,
+            type: typeSelect === 'income' ? 'Thu nhập' : 'Chi tiêu',
+            category: editedCategory,
+            amount: Number(editedAmount),
+            date: editedDate.split('-').reverse().join('-'),
+            categoryIcon: `http://localhost:8080/api/images/${chosenCategoryIcon}.png`
+        };
+
+        fetch(`http://localhost:8080/api/update-transaction/${editingId}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updatedTransaction),
+        })
+            .then(res => res.json())
+            .then(updatedData => {
+                setTransList(updatedData);
+                setAddEdit(0);
+                setEditingId(null);
+                setEditedNote('');
+                setEditedAmount('');
+                setEditedDate('');
+                setEditedCategory('Lương');
+            })
+            .catch(err => console.error("Lỗi khi sửa: ", err));
+    }
+
+    const [selectedIds, setSelectedIds] = useState([]); // Danh sách Id để xóa
+
+    // Xử lý khi tick chọn từng ô checkbox
+    const handleCheckboxChange = (id) => {
+        setSelectedIds(prev =>
+            prev.includes(id) ? prev.filter(itemId => itemId !== id) : [...prev, id]
+        );
+    };
+
+    // Chọn tất cả
+    const handleSelectAll = () => {
+        const allFilteredIds = filteredTransList.map(item => item.id);
+        setSelectedIds(allFilteredIds);
+    };
+
+    // Bỏ chọn tất cả
+    const handleUnselectedAll = () => {
+        setSelectedIds([]);
+    }
+
+    // Hàm xóa
+    const handleDeleteSelected = () => {
+        if (selectedIds.length === 0) {
+            alert("Vui lòng chọn ít nhất một giao dịch để xóa!");
+            return;
+        }
+
+        if (window.confirm(`Bạn có chắc muốn xóa ${selectedIds.length} giao dịch đã chọn?`)) {
+            fetch('http://localhost:8080/api/delete-transactions', {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ ids: selectedIds }) // Gửi mảng ID
+            })
+                .then(res => {
+                    if (!res.ok) throw new Error("Lỗi khi xóa từ server");
+                    return res.json();
+                })
+                .then(updatedData => {
+                    // Backend trả về danh sách mới sau khi xóa
+                    setTransList(updatedData);
+                    setSelectedIds([]); // Reset danh sách đã chọn
+                    alert("Xóa thành công!");
+                })
+                .catch(err => {
+                    console.error("Lỗi xóa:", err);
+                    alert("Không thể xóa giao dịch, vui lòng thử lại.");
+                });
+        }
+    }
+
     return (
         <div className={styles['outer-boundary']} >
             <div className={styles['filter']} >
@@ -243,7 +396,23 @@ export function Transactions() {
                                 {item.type === 'Thu nhập' ? `+ ` : `- `}{`${item.amount} đ`}
                             </p>
                             <p>{item.date}</p>
-                            <input type={addEdit === 2 ? 'radio' : 'checkbox'} name='trans-item' value={item.id} className={styles['t-checkbox']} />
+                            <input type={addEdit === 2 ? 'radio' : 'checkbox'}
+                                // Tách luồng tránh xung đột
+                                name={addEdit === 2 ? 'edit-item' : 'delete-item'}
+                                value={item.id}
+                                className={styles['t-checkbox']}
+                                // Đang sửa thì dùng editingId, ngược lại dùng mảng selectedIds
+                                checked={addEdit === 2 ? editingId === item.id : selectedIds.includes(item.id)}
+                                // Ghi nhớ: checked={false} sẽ khiến checkbox không bị tick
+                                // React quan tâm cả giá trị đằng sau, không như HTML
+                                onChange={() => {
+                                    if (addEdit === 2) {
+                                        handleRadioChange(item.id);
+                                    } else {
+                                        handleCheckboxChange(item.id);
+                                    }
+                                }}
+                            />
                         </li>
                     ))}
                 </ul>
@@ -297,51 +466,57 @@ export function Transactions() {
                 </div>}
                 {addEdit === 2 && <div className={styles['editting-section']} >
                     <p><strong>Sửa giao dịch hiện tại</strong></p>
-                    <input type='text' style={{
-                        border: '2px solid rgb(0, 117, 70)',
-                        borderRadius: '20px',
-                        height: '22px',
-                        marginTop: '-4px',
-                        marginRight: '10px',
-                        paddingLeft: '5px',
-                        width: '325px',
-                    }} placeholder='Sửa tên giao dịch' />
-                    <select name='type-select' id='type-select' className={styles['filt-selector']} value={typeSelect} onChange={handleTypeSelect} >
-                        <option value='income' >Thu nhập</option>
-                        <option value='expense'>Chi phí</option>
-                    </select>
-                    <select name='category-select' id='category-select' style={{ width: '120px' }} className={styles['filt-selector']}>
-                        {typeSelect === 'expense' ? (<>
-                            <option value='Ăn uống' >Ăn uống</option>
-                            <option value='Đơn điện tử' >Đơn điện tử</option>
-                            <option value='Sức khỏe' >Sức khỏe</option>
-                            <option value='Nhà ở' >Nhà ở</option>
-                            <option value='Đi lại' >Đi lại</option>
-                            <option value='Giải trí' >Giải trí</option>
-                            <option value='Mua sắm' >Mua sắm</option>
-                            <option value='Chi tiêu khác' >Chi tiêu khác</option>
-                        </>) : (<>
-                            <option value='Lương' >Lương</option>
-                            <option value='Thu nhập khác' >Thu nhập khác</option>
-                        </>)}
-                    </select>
-                    <input type='text' style={{
-                        border: '2px solid rgb(0, 117, 70)',
-                        borderRadius: '20px',
-                        height: '22px',
-                        marginTop: '-4px',
-                        marginRight: '10px',
-                        paddingLeft: '5px',
-                    }} placeholder='Số tiền' />
-                    <input type='date' style={{
-                        border: '2px solid rgb(0, 117, 70)',
-                        borderRadius: '20px',
-                        height: '22px',
-                        marginTop: '-4px',
-                        marginRight: '10px',
-                        paddingLeft: '5px',
-                    }} />
-                    <button className={styles['func-button']} >Sửa giao dịch</button>
+                    {!editingId ? (
+                        <p style={{ color: 'green' }}>Vui lòng bấm chọn vào ô tròn bên cạnh 1 giao dịch để sửa.</p>
+                    ) : (<>
+                        <input type='text' style={{
+                            border: '2px solid rgb(0, 117, 70)',
+                            borderRadius: '20px',
+                            height: '22px',
+                            marginTop: '-4px',
+                            marginRight: '10px',
+                            paddingLeft: '5px',
+                            width: '325px',
+                        }} placeholder='Sửa tên giao dịch' value={editedNote} onChange={(e) => setEditedNote(e.target.value)} />
+                        <select name='type-select' id='type-select' className={styles['filt-selector']} value={typeSelect} onChange={handleTypeSelect} >
+                            <option value='income' >Thu nhập</option>
+                            <option value='expense'>Chi phí</option>
+                        </select>
+                        <select name='category-select' id='category-select' style={{ width: '120px' }} className={styles['filt-selector']} value={editedCategory} onChange={(e) => setEditedCategory(e.target.value)} >
+                            {typeSelect === 'expense' ? (<>
+                                <option value='Ăn uống' >Ăn uống</option>
+                                <option value='Đơn điện tử' >Đơn điện tử</option>
+                                <option value='Sức khỏe' >Sức khỏe</option>
+                                <option value='Nhà ở' >Nhà ở</option>
+                                <option value='Đi lại' >Đi lại</option>
+                                <option value='Giải trí' >Giải trí</option>
+                                <option value='Mua sắm' >Mua sắm</option>
+                                <option value='Chi tiêu khác' >Chi tiêu khác</option>
+                            </>) : (<>
+                                <option value='Lương' >Lương</option>
+                                <option value='Thu nhập khác' >Thu nhập khác</option>
+                            </>)}
+                        </select>
+                        <input type='text' style={{
+                            border: '2px solid rgb(0, 117, 70)',
+                            borderRadius: '20px',
+                            height: '22px',
+                            marginTop: '-4px',
+                            marginRight: '10px',
+                            paddingLeft: '5px',
+                        }} placeholder='Số tiền' value={editedAmount} onChange={(e) => setEditedAmount(e.target.value)} />
+                        <input type='date' style={{
+                            border: '2px solid rgb(0, 117, 70)',
+                            borderRadius: '20px',
+                            height: '22px',
+                            marginTop: '-4px',
+                            marginRight: '10px',
+                            paddingLeft: '5px',
+                        }} value={editedDate} onChange={(e) => setEditedDate(e.target.value)} />
+                        <button className={styles['func-button']} onClick={handleConfirmEdit} >Sửa giao dịch</button>
+                        <button onClick={() => setEditingId(null)} className={styles['func-button']} >Hủy chọn</button>
+                    </>
+                    )}
                 </div>}
             </div>
             <div className={styles['func-list']} >
@@ -349,9 +524,9 @@ export function Transactions() {
                     <strong style={{ marginRight: '20px' }} >Chức năng</strong>
                     <button className={styles['func-button']} onClick={handleAddButton} >Thêm mới</button>
                     <button className={styles['func-button']} onClick={handleEditButton} >Sửa giao dịch</button>
-                    <button className={addEdit === 2 ? styles['delete-button-unactive'] : styles['delete-button']} >Xóa</button>
-                    <button className={addEdit === 2 ? styles['tickall-button-unactive'] : styles['tickall-button']} >Chọn hết</button>
-                    <button className={addEdit === 2 ? styles['untickall-button-unactive'] : styles['untickall-button']} >Bỏ chọn hết</button>
+                    <button className={addEdit === 2 ? styles['delete-button-unactive'] : styles['delete-button']} onClick={handleDeleteSelected} disabled={addEdit === 2} >Xóa</button>
+                    <button className={addEdit === 2 ? styles['tickall-button-unactive'] : styles['tickall-button']} onClick={handleSelectAll} disabled={addEdit === 2} >Chọn hết</button>
+                    <button className={addEdit === 2 ? styles['untickall-button-unactive'] : styles['untickall-button']} onClick={handleUnselectedAll} disabled={addEdit === 2} >Bỏ chọn hết</button>
                     <label htmlFor='sorter' style={{ marginLeft: '10px', marginRight: '10px' }} >Sắp xếp:</label>
                     <select name='sorter' id='sorter' className={styles['filt-selector']} >
                         <option value='none' >Không sắp xếp</option>
