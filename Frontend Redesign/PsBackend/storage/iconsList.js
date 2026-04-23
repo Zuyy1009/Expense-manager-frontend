75ae5443e8007f84644464397bbcb71c28cfe8d6
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-23 16:06:12 +0700

    Create iconsList.js

---
+++ Frontend Redesign/PsBackend/storage/iconsList.js
@@ -0,0 +1,17 @@
+const getIconName = (category) => {
+    const iconMap = {
+        'Ăn uống': 'eating',
+        'Đơn điện tử': 'elecbill',
+        'Sức khỏe': 'health',
+        'Nhà ở': 'housing',
+        'Đi lại': 'movement',
+        'Giải trí': 'recreation',
+        'Mua sắm': 'shopping',
+        'Chi tiêu khác': 'otherexpense',
+        'Lương': 'salary',
+        'Thu nhập khác': 'otherincome',
+    };
+    return iconMap[category] || 'salary';
+};
+
+module.exports = {getIconName};
