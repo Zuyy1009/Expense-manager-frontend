1e64a95c75d9726f5fa73726b81a8ea0b04383cf
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-23 14:56:37 +0700

    Add Account component with basic structure

---
+++ Frontend Redesign/Frontend/src/account/account.jsx
@@ -0,0 +1,9 @@
+import styles from './account.module.css';
+
+export function Account() {
+    return (
+        <div className={styles['outer-boundary']} >
+            Account Setting
+        </div>
+    )
+}
