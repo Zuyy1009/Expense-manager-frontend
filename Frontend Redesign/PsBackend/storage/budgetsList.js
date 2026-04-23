5a60d288dd466e7258539c8ecd51b7119a619ef9
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-23 19:35:04 +0700

    Refactor budgetsList to include category icons

--- Frontend Redesign/PsBackend/storage/budgetsList.js
+++ Frontend Redesign/PsBackend/storage/budgetsList.js
@@ -1,11 +1,83 @@
 const { getIconName } = require('./iconsList.js');
 
-let budgsList = [
+let budgsListWithoutIcon = [
     {
-        id: 1,
+        id: '26Ap-1',
         category: 'Ăn uống',
-        month: 'April'
+        month: 'April',
+        year: '2026',
+        limitAmount: 500000,
+        alertThreshold: 80,
+        isActive: true,
+    },
+    {
+        id: '26Ap-2',
+        category: 'Đơn điện tử',
+        month: 'April',
+        year: '2026',
+        limitAmount: 300000,
+        alertThreshold: 70,
+        isActive: true,
+    },
+    {
+        id: '26Ap-3',
+        category: 'Sức khỏe',
+        month: 'April',
+        year: '2026',
+        limitAmount: 500000,
+        alertThreshold: 50,
+        isActive: true,
+    },
+    {
+        id: '26Ap-4',
+        category: 'Nhà ở',
+        month: 'April',
+        year: '2026',
+        limitAmount: 600000,
+        alertThreshold: 70,
+        isActive: true,
+    },
+    {
+        id: '26Ma-1',
+        category: 'Ăn uống',
+        month: 'March',
+        year: '2026',
+        limitAmount: 500000,
+        alertThreshold: 80,
+        isActive: true,
+    },
+    {
+        id: '26Ma-2',
+        category: 'Đơn điện tử',
+        month: 'March',
+        year: '2026',
+        limitAmount: 300000,
+        alertThreshold: 70,
+        isActive: false,
+    },
+    {
+        id: '26Ma-3',
+        category: 'Sức khỏe',
+        month: 'March',
+        year: '2026',
+        limitAmount: 500000,
+        alertThreshold: 50,
+        isActive: true,
+    },
+    {
+        id: '26Ma-4',
+        category: 'Nhà ở',
+        month: 'March',
+        year: '2026',
+        limitAmount: 600000,
+        alertThreshold: 70,
+        isActive: false,
     },
 ];
 
-module.exports = {budgsList};
+let budgsList = budgsListWithoutIcon.map(item => ({
+    ...item,
+    categoryIcon: `http://localhost:8080/api/images/${getIconName(item.category)}.png`
+}));
+
+module.exports = { budgsList };
