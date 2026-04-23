2beb674378c5a4975274fe39b5d63c6f15693d2c
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-23 16:06:53 +0700

    Create budgetsList.js

---
+++ Frontend Redesign/PsBackend/storage/budgetsList.js
@@ -0,0 +1,11 @@
+const { getIconName } = require('./iconsList.js');
+
+let budgsList = [
+    {
+        id: 1,
+        category: 'Ăn uống',
+        month: 'April'
+    },
+];
+
+module.exports = {budgsList};
