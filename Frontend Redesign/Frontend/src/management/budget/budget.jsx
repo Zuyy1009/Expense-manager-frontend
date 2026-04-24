118090b392f68685add09a67e627cdf8ad2c496d
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-24 08:58:10 +0700

    Refactor budget state management and UI elements

--- Frontend Redesign/Frontend/src/management/budget/budget.jsx
+++ Frontend Redesign/Frontend/src/management/budget/budget.jsx
@@ -2,16 +2,28 @@ import styles from './budget.module.css'
 import { useState, useEffect } from 'react'
 
 export function Budget() {
-    const [budgetList, setBudgetList] = useState([]);
+    const [budgetsList, setBudgetsList] = useState([]);
+    const [iconsMap, setIconsMap] = useState({});
 
     useEffect(() => {
         fetch("http://localhost:8080/api/budgetslist")
             .then(res => res.json())
             .then(data => {
-                setBudgetList(data);
+                setBudgetsList(data);
             })
             .catch(err => console.error("Đã xảy ra lỗi!: ", err));
     }, []);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/iconslist")
+            .then(res => res.json())
+            .then(data => {
+                setIconsMap(data);
+            })
+            .catch(err => console.error("Đã xảy ra lỗi!: ", err));
+    }, []);
+
+    const filteredBudgetsList = budgetsList;
 
     return (
         <div>
@@ -20,9 +32,10 @@ export function Budget() {
             <section className={styles['outer-boundary']} >
                 <div className={styles['budget-region']} >
                     <section className={styles['filter-section']} >
-                        <p style={{color: 'rgb(69, 74, 73)', display: 'inline', marginRight: '20px'}} ><strong>Bộ lọc</strong></p>
+                        <p style={{ color: 'rgb(69, 74, 73)', display: 'inline', marginRight: '20px' }} ><strong>Bộ lọc</strong></p>
                         <label htmlFor='month' >Tháng</label>
-                        <select name='month' id='month' className={styles['filt-selector']} style={{width: '90px'}} >
+                        <select name='month' id='month' className={styles['filt-selector']} style={{ width: '100px' }} >
+                            <option value='all-months' >Tất cả tháng</option>
                             <option value='01' >Tháng 1</option>
                             <option value='02' >Tháng 2</option>
                             <option value='03' >Tháng 3</option>
@@ -37,7 +50,8 @@ export function Budget() {
                             <option value='12' >Tháng 12</option>
                         </select>
                         <label htmlFor='year' >Năm</label>
-                        <select name='year' id='year' className={styles['filt-selector']} style={{width: '70px'}} >
+                        <select name='year' id='year' className={styles['filt-selector']} style={{ width: '100px' }} >
+                            <option value='all-years' >Tất cả năm</option>
                             <option value='2026' >2026</option>
                             <option value='2025' >2025</option>
                             <option value='2024' >2024</option>
@@ -45,12 +59,60 @@ export function Budget() {
                             <option value='2022' >2022</option>
                         </select>
                         <label htmlFor='state' >Trạng thái</label>
-                        <select name='state' id='state' className={styles['filt-selector']} style={{width: '130px'}} >
+                        <select name='state' id='state' className={styles['filt-selector']} style={{ width: '130px' }} >
+                            <option value='all-states'>Tất cả trạng thái</option>
                             <option value='active' >Hoạt động</option>
                             <option value='non-active' >Không hoạt động</option>
                         </select>
                     </section>
                     <hr style={{ marginTop: '6px' }} />
+                    <section className={styles['budgs-list-region']} >
+                        <div className={styles['bsl-boundary']} >
+                            <ul className={styles['budgs-list']} style={{ listStyle: 'none' }} >
+                                {filteredBudgetsList.map((item, index) => (
+                                    <li style={{
+                                        display: 'grid',
+                                        gridTemplateColumns: '30px 160px 75px 80px 110px 80px 180px 50px',
+                                        gridAutoRows: '35px',
+                                        border: '2px solid rgb(0, 117, 70)',
+                                        borderRadius: '20px',
+                                        backgroundColor: 'white',
+                                        paddingLeft: '15px',
+                                        marginBottom: '5px'
+                                    }} >
+                                        <p><strong>{index + 1}</strong></p>
+                                        <p>{item.category}</p>
+                                        <p>{item.month}</p>
+                                        <p>{item.year}</p>
+                                        <p>{item.limitAmount}</p>
+                                        <p>{item.alertThreshold}</p>
+                                        {item.isActive === true ?
+                                            <p style={{ color: 'green' }} ><strong>Hoạt động</strong></p> :
+                                            <p style={{ color: 'grey' }} ><strong>Không hoạt động</strong></p>}
+                                        <p>{item.amountConsumed}</p>
+                                        {item.isActive === true ?
+                                            <div style={{
+                                                width: '300px',
+                                                height: '15px',
+                                                backgroundColor: 'rgb(217, 239, 233)',
+                                                border: '2px solid rgb(46, 158, 115)',
+                                                borderRadius: '20px',
+                                                marginTop: '10px'
+                                            }} ></div> :
+                                            <div style={{
+                                                width: '300px',
+                                                height: '15px',
+                                                backgroundColor: 'rgb(205, 205, 205)',
+                                                border: '2px solid rgb(174, 174, 174)',
+                                                borderRadius: '20px',
+                                                marginTop: '10px'
+                                            }} ></div>}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </section>
+                    <hr style={{ marginTop: '3px' }} />
                 </div>
                 <div className={styles['stat-region']} ></div>
             </section>
