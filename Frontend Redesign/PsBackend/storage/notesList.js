8e23cf185dd4831fb0785b4f27ad4e2f8ec7340d
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-25 22:20:21 +0700

    Add notesList.js to manage notes data

---
+++ Frontend Redesign/PsBackend/storage/notesList.js
@@ -0,0 +1,24 @@
+let nsList = [
+    {
+        id: 1,
+        content: 'Chi tiêu phần ăn uống ít lại',
+    },
+    {
+        id: 2,
+        content: 'Thiết lập aabb',
+    },
+    {
+        id: 3,
+        content: 'Thiết lập aabb',
+    },
+    {
+        id: 4,
+        content: 'Thiết lập aabb',
+    },
+    {
+        id: 5,
+        content: 'Thiết lập aabb',
+    },
+];
+
+module.exports = { nsList };
