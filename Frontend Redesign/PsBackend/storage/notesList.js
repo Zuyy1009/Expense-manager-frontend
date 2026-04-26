d1a1c4f99da4e8ee2c05b1c8c1250386ca92d8d0
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-26 10:12:58 +0700

    Add titles to notes in notesList.js

--- Frontend Redesign/PsBackend/storage/notesList.js
+++ Frontend Redesign/PsBackend/storage/notesList.js
@@ -1,23 +1,28 @@
 let nsList = [
     {
         id: 1,
-        content: 'Chi tiêu phần ăn uống ít lại',
+        title: 'Note A',
+        content: 'Chi tiêu phần ăn uống ít lại'
     },
     {
         id: 2,
-        content: 'Thiết lập aabb',
+        title: 'Note B',
+        content: 'Thiết lập aabb'
     },
     {
         id: 3,
-        content: 'Thiết lập aabb',
+        title: 'Note A',
+        content: 'On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks.'
     },
     {
         id: 4,
-        content: 'Thiết lập aabb',
+        title: 'Note B',
+        content: 'Thiết lập aabb,On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks.'
     },
     {
         id: 5,
-        content: 'Thiết lập aabb',
+        title: 'Note A',
+        content: 'Thiết lập aabb'
     },
 ];
 
