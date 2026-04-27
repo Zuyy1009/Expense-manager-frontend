cd84de4a3d7a930adc71914cfea7ddd4bdb6cda6
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-04-27 19:18:08 +0700

    Refactor categoriesList to calculate amountMade dynamically

--- Frontend Redesign/PsBackend/storage/categoriesList.js
+++ Frontend Redesign/PsBackend/storage/categoriesList.js
@@ -6,7 +6,6 @@ let catesListWithoutIcon = [
         id: 1,
         name: 'Ăn uống',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Ăn uống').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -14,7 +13,6 @@ let catesListWithoutIcon = [
         id: 2,
         name: 'Đơn điện tử',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Đơn điện tử').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -22,7 +20,6 @@ let catesListWithoutIcon = [
         id: 3,
         name: 'Sức khỏe',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Sức khỏe').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -30,7 +27,6 @@ let catesListWithoutIcon = [
         id: 4,
         name: 'Nhà ở',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Nhà ở').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -38,7 +34,6 @@ let catesListWithoutIcon = [
         id: 5,
         name: 'Đi lại',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Đi lại').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -46,7 +41,6 @@ let catesListWithoutIcon = [
         id: 6,
         name: 'Giải trí',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Giải trí').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -54,7 +48,6 @@ let catesListWithoutIcon = [
         id: 7,
         name: 'Mua sắm',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Mua sắm').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -62,7 +55,6 @@ let catesListWithoutIcon = [
         id: 8,
         name: 'Chi tiêu khác',
         type: 'Chi tiêu',
-        amountMade: transList.filter(i => i.category === 'Chi tiêu khác').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -70,7 +62,6 @@ let catesListWithoutIcon = [
         id: 9,
         name: 'Lương',
         type: 'Thu nhập',
-        amountMade: transList.filter(i => i.category === 'Lương').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -78,7 +69,6 @@ let catesListWithoutIcon = [
         id: 10,
         name: 'Thu nhập khác',
         type: 'Thu nhập',
-        amountMade: transList.filter(i => i.category === 'Thu nhập khác').reduce((s, i) => s + i.amount, 0),
         isDefault: true,
         createdAt: '20-12-2022'
     },
@@ -86,7 +76,8 @@ let catesListWithoutIcon = [
 
 let catesList = catesListWithoutIcon.map(item => ({
     ...item,
-    icon: `http://localhost:8080/api/images/${iconsMap[item.name]}.png`
+    icon: `http://localhost:8080/api/images/${iconsMap[item.name]}.png`,
+    amountMade: transList.filter(i => i.category === item.name).reduce((s, i) => s + i.amount, 0)
 }));
 
 module.exports = { catesList };
