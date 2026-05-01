f91fec69c63acf978ef93c6997b9544f6458c75c
CrabbieOka <phamnguyenduyanh09@gmail.com>
2026-05-01 10:35:34 +0700

    Add userId field to Transaction schema

--- Frontend Redesign/PsBackend/models/Transaction.js
+++ Frontend Redesign/PsBackend/models/Transaction.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
+    userId: mongoose.Schema.Types.ObjectId,
     type: String,
     amount: Number,
     category: String,
